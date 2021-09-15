51963eae52b237fa0342121ab67a0a7fb2a10a8e
SergioGenesis97 <sergio.bautista@alumnos.udg.mx>
2021-09-15 17:01:38 -0500

    Se solucionaron algunos errores conocidos

--- resources/views/templates/layouts/header.blade.php
+++ resources/views/templates/layouts/header.blade.php
@@ -17,8 +17,8 @@ Coded by www.creative-tim.com
 
 <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="./../../../../public/img//apple-icon.png">
-    <link rel="icon" type="image/png" href="./../../../../public/img//favicon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="./../../../img//apple-icon.png">
+    <link rel="icon" type="image/png" href="./../../../img//favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     
     <title>
@@ -48,7 +48,7 @@ Coded by www.creative-tim.com
             <div class="logo">
                 <a href="https://www.creative-tim.com" class="simple-text logo-mini">
                     <div class="logo-image-small">
-                        <img src="./../../../../public/img/logo-small.png">
+                        <img src="./../../../img/logo-small.png">
                     </div>
                     <!-- <p>CT</p> -->
                 </a>
@@ -62,7 +62,7 @@ Coded by www.creative-tim.com
             <div class="sidebar-wrapper">
                 <div class="user">
                     <div class="photo">
-                        <img src="./../../../../public/img/Escudo U de G.png" />
+                        <img src="./../../../img/Escudo U de G.png" />
                     </div>
                     <div class="info">
                         <a data-toggle="collapse" href="#collapseExample" class="collapsed">
