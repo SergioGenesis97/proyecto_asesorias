aa030aaa20e543ad13b7daa9d280cd7e3165a3bb
SergioGenesis97 <sergio.bautista@alumnos.udg.mx>
2021-09-15 15:14:23 -0500

    Se añadieron plantillas

---
+++ resources/views/templates/layouts/footer.blade.php
@@ -0,0 +1,81 @@
+
+            <!-- Footer -->
+            <footer class="footer footer-black  footer-white ">
+                <div class="container-fluid">
+                    <div class="row">
+                        <nav class="footer-nav">
+                            <ul>
+                                <li><a href="http://www.web.valles.udg.mx/" target="_blank">CUValles Of.</a></li>
+                                <li><a href="https://www.udg.mx/es/centros-universitarios/cuvalles" target="_blank">U. de G. CUValles</a></li>
+                                <li><a href="http://siiauescolar.siiau.udg.mx/wus/gupprincipal.inicio" target="_blank">SIIAU</a></li>
+                            </ul>
+                        </nav>
+                        <div class="credits ml-auto">
+                            <span class="copyright">
+                                ©
+                                <script>
+                                    document.write(new Date().getFullYear())
+                                </script>, made with <i class="fa fa-heart heart"></i> by Creative Tim
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <!--   Core JS Files   -->
+    <script src="./../../src/js/core/jquery.min.js"></script>
+    <script src="./../../src/js/core/popper.min.js"></script>
+    <script src="./../../src/js/core/bootstrap.min.js"></script>
+    <script src="./../../src/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+    <script src="./../../src/js/plugins/moment.min.js"></script>
+    <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+    <script src="./../../src/js/plugins/bootstrap-switch.js"></script>
+    <!--  Plugin for Sweet Alert -->
+    <script src="./../../src/js/plugins/sweetalert2.min.js"></script>
+    <!-- Forms Validations Plugin -->
+    <script src="./../../src/js/plugins/jquery.validate.min.js"></script>
+    <!--  Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
+    <script src="./../../src/js/plugins/jquery.bootstrap-wizard.js"></script>
+    <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
+    <script src="./../../src/js/plugins/bootstrap-selectpicker.js"></script>
+    <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
+    <script src="./../../src/js/plugins/bootstrap-datetimepicker.js"></script>
+    <!--  DataTables.net Plugin, full documentation here: https://datatables.net/    -->
+    <script src="./../../src/js/plugins/jquery.dataTables.min.js"></script>
+    <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
+    <script src="./../../src/js/plugins/bootstrap-tagsinput.js"></script>
+    <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
+    <script src="./../../src/js/plugins/jasny-bootstrap.min.js"></script>
+    <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
+    <script src="./../../src/js/plugins/fullcalendar/fullcalendar.min.js"></script>
+    <script src="./../../src/js/plugins/fullcalendar/daygrid.min.js"></script>
+    <script src="./../../src/js/plugins/fullcalendar/timegrid.min.js"></script>
+    <script src="./../../src/js/plugins/fullcalendar/interaction.min.js"></script>
+    <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
+    <script src="./../../src/js/plugins/jquery-jvectormap.js"></script>
+    <!--  Plugin for the Bootstrap Table -->
+    <script src="./../../src/js/plugins/nouislider.min.js"></script>
+    <!--  Google Maps Plugin    -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+    <!-- Chart JS -->
+    <script src="../assets/js/plugins/chartjs.min.js"></script>
+    <!--  Notifications Plugin    -->
+    <script src="./../../src/js/plugins/bootstrap-notify.js"></script>
+    <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="./../../src/js/paper-dashboard.min.js?v=2.1.1" type="text/javascript"></script>
+    <!-- Paper Dashboard DEMO methods, don't include it in your project! -->
+    <script src="./../../src/demo/demo.js"></script>
+    <script>
+        $(document).ready(function () {
+            // Javascript method's body can be found in assets/js/demos.js
+            demo.initDashboardPageCharts();
+
+
+            demo.initVectorMap();
+
+        });
+    </script>
+</body>
+
+</html>
